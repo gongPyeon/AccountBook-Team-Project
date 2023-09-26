e947f61e14151653dd6ea782f5375fc96b64df1b
Grape147 <jhg5130621@gmail.com>
2023-09-26 20:52:24 +0900

    Process1
    Process1과 Dao에 있는  sql selection문만 수정했습니다

---
+++ TestMain.java
@@ -0,0 +1,11 @@
+package AccountBook;
+
+public abstract class TestMain {
+
+	public static void main(String[] args) {
+		AccountBookDao dao  = new AccountBookDao();
+		Process1 aa = new Process1();
+
+	}
+
+}
