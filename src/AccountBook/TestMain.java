5b08a6ca294660a1ed4ca7cac27a0352cee279b1
Hyeri1ee <haerizian10@gmail.com>
2023-10-30 20:17:12 +0900

    AccountBookDao 에 process1 함수 추가

--- src/AccountBook/TestMain.java
+++ src/AccountBook/TestMain.java
@@ -1,14 +1,12 @@
 package AccountBook;
 
-import java.util.ArrayList;
-
 public abstract class TestMain {
 
 	public static void main(String[] args) {
 		AccountBookDao dao  = new AccountBookDao();
-//		MainProgram main  = new MainProgram();
-		ArrayList<AccountBookVO> a;
-		
+		//dao.delete_schedule(1);
+		MainProgram main = new MainProgram();
+
 	}
 
 }
