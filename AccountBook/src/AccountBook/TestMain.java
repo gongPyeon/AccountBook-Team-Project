1b6f92047e8c9982ba54375fc51cfeaa8a78c88b
Rollybrother <87607046+Rollybrother@users.noreply.github.com>
2023-11-04 22:27:20 +0900

    Update TestMain.java
    최종

--- AccountBook/src/AccountBook/TestMain.java
+++ AccountBook/src/AccountBook/TestMain.java
@@ -3,8 +3,6 @@ package AccountBook;
 public abstract class TestMain {
 
 	public static void main(String[] args) {
-		AccountBookDao dao  = new AccountBookDao();
-		//dao.delete_schedule(1);
 		MainProgram main = new MainProgram();
 
 	}
